290a99ed46be12098e82001d12ccbb9453ca17df
Aleksei Gutikov <gutikoff@gmail.com>
2019-10-02 19:19:15 +0300

    wip queries

--- cl_hw_opt.py
+++ cl_hw_opt.py
@@ -177,10 +177,11 @@ class ObjectType:
         
         # List of requirements
         # Each requirement contains:
-        #   query:
-        #     selector of single object
-        #     resource name
-        #   value
+        #   resource name - string, key of dict
+        #   query - returns objects to select resource source from
+        #   value of required resource - simple integer
+        #
+        # For every requirements scheduler 
         self.requires = {}
         
         # 
@@ -202,7 +203,6 @@ global_properties = {
     # (name, predicate) : query
 }
 
-#TODO: Consider initial ObjectType as immutable and generate usable OpbjectType with properties added, categories parsed and queries compiled
 
 def add_property_if_applicable(obj_type: ObjectType, name: str, predicate: str, query: str):
     if name in obj_type.properties or name in obj_type.provides:
@@ -212,6 +212,12 @@ def add_property_if_applicable(obj_type: ObjectType, name: str, predicate: str, 
     if p(cats):
         obj_type.properties[name] = query
 
+
+def preprocess_object_types(type_list: List[ObjectType]) -> List[ObjectType]:
+    """
+        Consider initial ObjectType as immutable and generate usable ObjectType with properties added, categories parsed and queries compiled
+    """
+    return []
 
 
 
@@ -238,26 +244,113 @@ class Object:
         self.used_resources = {}
 
         # uuid of parent object where this object is in objects list
-        self.parent = None
+        self.parent: Object = None
 
         # owning list of child objects
-        self.objects = []
+        self.objects: List[Object] = []
 
         # objects where this object uses resources by resource name, uplinks
-        self.uses = {} 
+        self.source: Dict[str, Object] = {} 
 
         # objects that use this object by resource that is consumed
-        self.users = {}
+        self.users: Dict[str, List[Object]] = {}
 
 
-
+objects_by_uuid : Dict[uuid.UUID, Object] = {}
 
 
 #TODO: Do not undestand how to use jq or jinja2 or any other embeddable lang here for queries
 # So first implement simple primitive hand-made one and then find a way to use others.
 
+
 """
 =====================================================================================================
+Query
+
+    Query vs. Equation?
+    Query is functional processing pipe starting on current object.
+    Equation is named formula that contains arithmetic operations, resource values on current object or global root.
+
+Pipe notation
+
+    List of functions separated with vertical bar "|".
+    Each function receive single argument or list and return single object, value or list.
+    To apply single-argument function to list - explicitly use "map".
+    Argument of first function is single current object.
+    Result of previous function pass to next one.
+    Output of last function is result of the query.
+
+Parentheses
+
+    Functions linked with pipe represent one function.
+    So just surround them with parentheses - it became single function that can be used with map or recursive.
+
+Path selector
+
+    Path selector cat takes objects or values.
+
+    Current object is argument for first function automatically - not required special sign for current object.
+    / - root object
+    .parent, ../ - parent object
+    .objects, .[] - child objects
+    .source.<res_name>, .src.<res_name> - source of <res_name> for this object
+    .users.<res_name> - users of <res_name> of this object
+    .resources.<res_name>, .res.<res_name> - value of total of <res_name>
+    .used.<res_name> - value of used of <res_name>
+    .free.<res_name> - value of free of <res_name>
+
+Map functor
+
+    If function return list of objects to apply next function to every object - use map:
+    Example - get cpu from every child object: .[] | map .res.cpu
+
+Join function
+
+    If mapped function return list for every object - then you got list of lists into output.
+    Join converts it into simple flat list.
+    It does nothing if not required.
+    Example - get cpu users of from every child object: .[] | map .users.cpu | join
+    TODO: consider apply join by default.
+
+Predicate
+
+
+Arithmetic function from values on single object
+
+
+
+
+
+Filter functor
+
+
+
+Functions on lists:
+    count
+    sum
+    min
+    max
+
+
+Recursive search
+
+    Path selector returns one object or list of objects or value or list of values.
+    If it is required to got through depth of links - use recursive search.
+    It recursively applies provided function to it's output while output is not empty.
+    And the whole function returns all objects that match predicate from every level.
+    Recursive functor takes two arguments:
+    1-st is required - function that will be applied recursively
+    2-nd is optional - filter predicate
+
+
+
+
+
+
+
+
+=====================================================================================================
+
 Tree Element Selector (object selector)
 
     Selector: path specifyer + type predicate + traverse mode + select mode, |-separated, repeatable
@@ -286,10 +379,8 @@ Query language
 
     Resource getter format: get <res_name>
 
-    Functions:
-        count
-        sum
-    
+
+
 
     query for resources - in properties
     query for object - in requirement
@@ -299,14 +390,52 @@ Query language
 
 
 
+class Value:
+    """
+        Consider equation V = n*(x - y)/(z + w) and constraint V >= 10.
+        If V will be calculated into numerical value and then constraint checked,
+        then we get no information about what free variable values we should increase or decrease
+        to finally match the constraint.
+
+        Then one way (that I see) is to link result value with all it's sources with labeled linkes.
+        Labels are proportionality direction: direct or inverse.
+        Also can be added labels like proportionality rank: linear, polynomial, exponential.
+        Using this links (I hope) solver will be able to infer what free variable should be increased or decreased.
+        And in this application all modifications of free variables, like add one more hdd disk,
+        will have cost in terms of optimization target value, like financial cost.
+        So solver (again, I hope) will be able to select modification that makes maximum(impact/cost). 
+
+    """
+    def __init__(self):
+        pass
+
+
+def compile_query(q: str):
+    pass
+
 
 
 
 """
 =====================================================================================================
+Variables
+
+    Global value that can vary during optimization process.
+
+
+
+Equations
+
+
+
+
 Constraints
+
+
+
 =====================================================================================================
 """
+
 
 
 
